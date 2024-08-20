4c56569e21ee0584d56d307fc4a83039c74f5a96
KuroHere <minhduc09925@gmail.com>
2024-08-20 08:37:41 +0700

    Changelogs #4 | Animals but no interface class 😥

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,11 @@ import impl.Dogs;
 
 public class Main {
 
-    public static Cats cats = new Cats(false);
+    public static Cats cats = new Cats(true);
     public static Dogs dogs = new Dogs(true);
 
     public static void main(String[] args) {
-        if (cats.hungry()) {
-            System.out.println(cats.sound());
-        }
-        if (dogs.hungry()) {
-            System.out.println(dogs.sound());
-        }
+        cats.actions();
+        dogs.actions();
     }
 }
